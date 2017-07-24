ec22ebf5d7e29d0df951399f9e9a6e98f4648ab5
Alexander Schaefer <acschaefer@users.noreply.github.com>
2017-07-24 22:42:35 +0200

    Add software angle limiter.

--- headlamp_control/headlight_trim.ino
+++ headlamp_control/headlight_trim.ino
@@ -22,21 +22,21 @@
 
 // Global constants and variables. /////////////////////////////////////////////
 // Pin numbers.
-const byte buttonUpPin = 2;//3;
+const byte buttonUpPin = 3;
 const byte buttonDownPin = 2;
 const byte wakePin = 0;
 const byte dirPin = 4;
 const byte stepPin = 1;
 
 // Step rate at maximum speed [Hz].
-const unsigned long stepRate = 23 * 200 / 4;
+const unsigned long stepRate = 23 * 200 / 3;
 
 // Duration needed to wake the motor driver and to set it asleep [us].
 const unsigned int wakeDuration = 1500u;
 const unsigned int sleepDuration = 100u;
 
 // Time the motor driver needs to recognize a step signal [us].
-const unsigned int stepDuration = 2u;
+const unsigned int stepDuration = 4u;
 
 // Point in time when the last step was triggered. The time is specified
 // relative to the time when the program was started [us].
@@ -44,6 +44,9 @@ unsigned long lastStep = 0ul;
 
 // Position of the motor in steps.
 int position = 0;
+
+// Maximum admissible position.
+const int maxPosition = 1.5 * 200 * 23;
 
 // Duration to full speed [us].
 unsigned long accelerationDuration = 500000ul;
@@ -80,29 +83,24 @@ void loop()
     // Read the states of the buttons.
     buttonUp.read();
     buttonDown.read();
-        
-    // Start or stop the motor.
-    if (buttonUp.wasPressed() || buttonUp.wasPressed())
+    
+    // Activate the motor.
+    if ((buttonUp.wasPressed() && position <= maxPosition)
+        || (buttonDown.wasPressed() && position >= -maxPosition))
     {
         digitalWrite(wakePin, HIGH);
         delayMicroseconds(wakeDuration);
-        turn = true;
         lastStep = micros();
         accelerationStart = lastStep;
     }
-    else if (buttonUp.wasReleased() || buttonDown.wasReleased())
-    {
-        digitalWrite(wakePin, LOW);
-        delayMicroseconds(sleepDuration);
-        turn = false;
-    }
     
     // Step the motor.
-    if (turn)
+    if ((position >= -maxPosition && buttonDown.isPressed())
+        || (position <= maxPosition && buttonUp.isPressed()))
     {   
         // Compute the current step rate [us].
         unsigned long elapsedAcceleration = micros() - accelerationStart;
-        unsigned long dt = (max(accelerationDuration / elapsedAcceleration, 1) 
+        unsigned long dt = (constrain(accelerationDuration / (double)elapsedAcceleration, 1.0, 1000.0) 
                             * 1000000) / stepRate;
 
         // Step the motor, if a step is due.
@@ -114,10 +112,17 @@ void loop()
             digitalWrite(stepPin, LOW);
 
             // Increment the position counter.
-            //position += buttonUp.isPressed() - buttonDown.isPressed();
+            position += buttonUp.isPressed() - buttonDown.isPressed();
 
             // Update the time of the last step command.
             lastStep += dt;
         }
-    }    
+    }
+
+    // Deactivate the motor.
+    if (buttonUp.wasReleased() || buttonDown.wasReleased())
+    {
+        digitalWrite(wakePin, LOW);
+        delayMicroseconds(sleepDuration);
+    }
 }
